ec493f3dd7ecae26de0d23ec06dda0f5529db02f
Lucia <aljojoab@uqvirtual.edu.co>
2025-02-04 17:15:03 -0500

    Blink

--- main.ino
+++ main.ino
@@ -1,8 +1,21 @@
-void setup()
-{
+// Definir el pin al que está conectado el LED
+int ledPin = 13;
 
+void setup() {
+  // Iniciar el pin del LED como salida
+  pinMode(ledPin, OUTPUT);
 }
-void loop ()
-{
 
+void loop() {
+  // Encender el LED
+  digitalWrite(ledPin, HIGH);
+  
+  // Esperar 1000 milisegundos (1 segundo)
+  delay(1000);
+  
+  // Apagar el LED
+  digitalWrite(ledPin, LOW);
+  
+  // Esperar 1000 milisegundos (1 segundo)
+  delay(1000);
 }
